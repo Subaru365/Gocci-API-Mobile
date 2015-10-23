0ff872006dd2e0cc3fb714c026e05d9231b0031c
Subaru365 <subaru.woody@gmail.com>
2015-10-23 14:49:46 +0900

    [refactoring] Validation Model
    validation.php split to validation, status and param for Model
    
    	modified:   fuel/app/classes/controller/v3/input.php
    	modified:   fuel/app/classes/model/v3/status.php
    	modified:   fuel/app/classes/model/v3/validation.php

--- fuel/app/classes/model/v3/status.php
+++ fuel/app/classes/model/v3/status.php
@@ -3,56 +3,87 @@
  * Status Code and Message list.
  *
  * @package    Gocci-Mobile
- * @version    3.0 (2015/10/21)
+ * @version    3.0 (2015/10/22)
  * @author     Subaru365 (a-murata@inase-inc.jp)
  * @license    MIT License
  * @copyright  2015 Inase,inc.
  * @link       https://bitbucket.org/inase/gocci-mobile-api
  */
 
-/** @return Array $status */
-class Model_V3_Status extends Model
+class Controller_V3_Status extends Controller
 {
-	/** @var Array $result */
-	private $result;
-
-
-	private function before()
-	{
-		$this->result = array(
+	/**
+	 * @param  String $status_code
+	 * @return Array  $status 
+	 */
+    public static function get_status($status_code)
+    {
+    	$status = array(
 			'version'   => 3.0,
-			'uri'       => Uri::string(),
-			'code'      => 'SUCCESS',
-			'message'   => 'Successful API request'
+			'uri'       => Uri::string()
 		);
-	}
 
-	private function SET_SUCCESS()
-	{
-		//$status = &$this->result;
-		$status = array_merge($status, array(
-			'code'      => 'SUCCESS',
-			'message'   => 'Successful API request',
-		));
-	}
+    	switch ($status_code) {
 
-	private function SET_ERROR_SESSION_EXPIRED()
-	{
-		array_merge($this->result, array(
-			'code' 		=> 'ERROR_SESSION_EXPIRED',
-			'message'   => 'Session cookie is not valid anymore'
-		));
-	}
+    		case 'SUCCESS':
+    			$status += array(
+    				'code'		=> 'SUCCESS',
+    				'message'	=> 'Successful API request'
+    			);
+    			break;
 
-	public function SUCCESS()
-	{
-		$this->SET_SUCCESS();
-		return $this->result;
-	}
+    		case 'ERROR_CONNECTION_FAILED':
+    			$status += array(
+    				'code'		=> 'ERROR_CONNECTION_FAILED',
+    				'message'	=> 'Server connection failed'
+    			);
+    			break;
 
-	public function ERROR_SESSION_EXPIRED()
-	{
-		$this->SET_ERROR_SESSION_EXPIRED();
-		return $this->result;
-	}
+    		case 'ERROR_USERNAME_ALREADY_REGISTERD':
+    			$status += array(
+    				'code'		=> 'ERROR_USERNAME_ALREADY_REGISTERD',
+    				'message'	=> 'The provided username was already registerd by another user'
+    			);
+    			break;
+
+    		case 'ERROR_REGISTER_ID_ALREADY_REGISTERD':
+				$status += array(
+    				'code'		=> 'ERROR_REGISTER_ID_ALREADY_REGISTERD',
+    				'message'	=> 'This deviced already has an registerd account'
+    			);
+    			break;
+
+    		case 'ERROR_IDENTITY_ID_NOT_REGISTERD':
+    			$status += array(
+    				'code'		=> 'ERROR_IDENTITY_ID_NOT_REGISTERD',
+    				'message'	=> 'The provided identity_id is not bound to any account'
+    			);
+    			break;
+
+    		case 'ERROR_USERNAME_NOT_REGISTERD':
+    			$status += array(
+    				'code'		=> 'ERROR_USERNAME_NOT_REGISTERD',
+    				'message'	=> 'The entered username does not exist'
+    			);
+    			break;
+
+    		case 'ERROR_PASSWORD_WRONG':
+    			$status += array(
+    				'code'		=> 'ERROR_PASSWORD_WRONG',
+    				'message'	=> 'Password wrong'
+    			);
+    			break;
+
+    		default:
+    			$status += array(
+    				'code'		=> 'ERROR_UNKNOWN_ERROR',
+    				'message'	=> 'Unknown global error'
+    			);
+    			break;
+    	}
+
+    	if ($status_code !== 'SUCCESS') {
+    		View::forge($status);
+    	}
+    }
 }
